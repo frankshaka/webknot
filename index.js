6eab9cde74201d972508965bdf8d2027950a9722
Frank Shaka <frankshaka@gmail.com>
2015-02-06 18:44:26 +0800

    Make request handler pass test

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+////////////////////////////////////////////////
+//
+// Web Request Handler
+//
+////////////////////////////////////////////////
+
+
+// Add 'startsWith' function to String objects
+// Reference: http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
 var http = require('http');
 var urlParser = require('url');
+var qs = require('querystring');
 
 var mainHTML = 'Gotcha!';
 
 var converters = {
 
     'test': function(data, headers) {
-        var targetURL = headers['x-target-url'] || 'http://requestb.in/1msdb5t1';
+        var url = headers['x-target-url'] || 'http://requestb.in/1msdb5t1';
         return {
-            url: targetURL,
-            data: data,
-            response: {
-                'url': targetURL
-            }
+            url: url,
+            format: 'json',
+            data: data
         };
     },
 
     'sns2slack': function(data, headers) {
         return {
             url: 'https://hooks.slack.com/services/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',
+            format: 'json',
             data: {
                 'text': '' + data['Subject'] + '\n\n' + data['Message']
             }
@@ -26,61 +41,117 @@ var converters = {
     }
 };
 
-function handleResponse(response) {
-    console.log('STATUS: ' + response.statusCode);
-    response.setEncoding('utf8');
-    response.on('data', function(chunk) {
-        console.log('BODY: ' + chunk);
+var formatters = {
+    'form': {
+        contentType: 'application/x-www-form-urlencoded',
+        stringify: qs.stringify,
+        parse: qs.parse
+    },
+    'json': {
+        contentType: 'application/json',
+        stringify: JSON.stringify,
+        parse: JSON.parse
+    }
+};
+
+var defaultFormatter = {
+    contentType: 'text/plain',
+    stringify: function(data) { return data; },
+    parse: function(data) { return data; }
+};
+
+function parseData(text, contentType) {
+    for (var k in formatters) {
+        if (contentType.startsWith(formatters[k].contentType)) {
+            return formatters[k].parse(text);
+        }
+    }
+    return defaultFormatter.parse(text);
+}
+
+
+function handleQueryRequest(request, response) {
+    if (request.location.pathname == '/') {
+        response.writeHead(200);
+        response.end(mainHTML);
+    }else {
+        response.writeHead(404);
+        response.end();
+    }
+}
+
+function handleWebhookRequest(request, response) {
+    var converterType = request.location.pathname.substr(1);
+    console.log('CONVERTER_TYPE: ' + converterType);
+    var convert = converters[converterType];
+    if (!convert) {
+        response.writeHead(404);
+        return response.end();
+    }
+
+    var requestBody = '';
+    request.on('data', function(chunk) {
+        requestBody += chunk;
+    });
+    request.on('end', function() {
+        console.log('REQUEST_BODY: ' + requestBody);
+        var contentType = request.headers['content-type'];
+        var requestData = parseData(requestBody, contentType);
+
+        var upstream = convert(requestData, request.headers);
+        if (!upstream) {
+            response.writeHead(400);
+            return response.end();
+        }
+
+        console.log('UPSTREAM OPTIONS: ' + JSON.stringify(upstream));
+
+        var upstreamRequestOptions = urlParser.parse(upstream.url);
+        upstreamRequestOptions.method = upstream.method || 'POST';
+        upstreamRequestOptions.headers = upstream.headers || {};
+
+        var formatter = formatters[upstream.format] || defaultFormatter;
+        var upstreamRequestBody = formatter.stringify(upstream.data);
+        upstreamRequestOptions.headers['Content-Type'] = formatter.contentType;
+        upstreamRequestOptions.headers['Content-Length'] = (upstreamRequestBody||'').length;
+
+        var upstreamRequest = http.request(upstreamRequestOptions, function(upstreamResponse) {
+            var upstreamResponseStatusCode = upstreamResponse.statusCode;
+            var upstreamResponseBody = '';
+            upstreamResponse.setEncoding('utf8');
+            upstreamResponse.on('data', function(chunk) {
+                upstreamResponseBody += chunk;
+            });
+            upstreamResponse.on('end', function() {
+                console.log('RESPONSE: [' + upstreamResponseStatusCode + '] ' + upstreamResponseBody);
+                response.writeHead(upstreamResponseStatusCode, {
+                    'Content-Type': upstreamResponse.headers['content-type']
+                });
+                response.write(upstreamResponseBody);
+                response.end();
+            });
+        });
+        upstreamRequest.on('error', function handleError(error) {
+            console.log('RESPONSE_ERROR: ' + error.message);
+            response.writeHead(500);
+            response.write(error.message);
+            response.end();
+        });
+        upstreamRequest.write(upstreamRequestBody);
+        upstreamRequest.end();
     });
 }
 
-function handleError(error) {
-    console.log('ERROR: ' + error.message);
-}
-
 function handleRequest(request, response) {
-    var urlData = urlParser.parse(request.url);
-    if (urlData.pathname == '/') {
-        response.statusCode = 200;
-        response.write(mainHTML);
+    request.location = urlParser.parse(request.url);
+    if (request.method == 'GET') {
+        return handleQueryRequest(request, response);
+    }else if (request.method == 'POST') {
+        return handleWebhookRequest(request, response);
     }else {
-        var converterId = urlData.pathname.substr(1);
-        var convert = converters[converterId];
-        if (convert) {
-            var contentType = request.headers['content-type'];
-            if (contentType == 'application/json') {
-                var data = JSON.parse(request.read());
-                var target = convert(data, request.headers);
-                if (target) {
-                    var targetRequestOptions = urlParser.parse(target.url);
-                    targetRequestOptions.method = target.method || 'POST';
-                    targetRequestOptions.headers = target.headers;
-
-                    var targetRequest = http.request(targetRequestOptions, handleResponse);
-                    targetRequest.on('error', handleError);
-                    targetRequest.write(
-                        typeof target.data == 'string' ? target.data : JSON.stringify(target.data)
-                    );
-                    targetRequest.end();
-                    response.statusCode = 200;
-
-                    if (target.response) {
-                        response.send(
-                            typeof target.response == 'string' ?
-                            target.response :
-                            JSON.stringify(target.response));
-                    }
-                }else {
-                    response.statusCode = 200;
-                }
-            }else {
-                response.statusCode = 400;
-            }
-        }else {
-            response.statusCode = 404;
-        }
+        response.writeHead(405);
+        response.end();
     }
-    response.end();
 }
 
 var port = process.env.PORT || 18080;
