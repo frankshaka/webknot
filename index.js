b72759432e26415858ceef11dd90ad6720bd33c1
Frank Shaka <frankshaka@gmail.com>
2015-02-06 11:18:05 +0800

    Add handler for '/test'.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var http = require('http');
 var urlParser = require('url');
 
+var mainHTML = 'Gotcha!';
+
 var converters = {
+
+    'test': function(data, headers) {
+        var targetURL = headers['x-target-url'] || 'http://requestb.in/1msdb5t1';
+        return {
+            url: targetURL,
+            data: data,
+            response: {
+                'url': targetURL
+            }
+        };
+    },
+
     'sns2slack': function(data, headers) {
         return {
             url: 'https://hooks.slack.com/services/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',
@@ -26,32 +40,42 @@ function handleError(error) {
 
 function handleRequest(request, response) {
     var urlData = urlParser.parse(request.url);
-    var converterId = urlData.pathname.substr(1);
-    var convert = converters[converterId];
-    if (convert) {
-        var contentType = request.headers['content-type'];
-        if (contentType == 'application/json') {
-
-            var data = JSON.parse(request.read());
-
-            var target = convert(data, request.headers);
-
-            var targetRequestOptions = urlParser.parse(target.url);
-            targetRequestOptions.method = target.method || 'POST';
-            targetRequestOptions.headers = target.headers;
-
-            var targetRequest = http.request(targetRequestOptions, handleResponse);
-            targetRequest.on('error', handleError);
-            targetRequest.write(
-                typeof target.data == 'string' ? target.data : JSON.stringify(target.data)
-            );
-            targetRequest.end();
-
-        }else {
-            response.statusCode = 400;
-        }
+    if (urlData.pathname == '/') {
+        response.statusCode = 200;
+        response.send(mainHTML);
     }else {
-        response.statusCode = 404;
+        var converterId = urlData.pathname.substr(1);
+        var convert = converters[converterId];
+        if (convert) {
+            var contentType = request.headers['content-type'];
+            if (contentType == 'application/json') {
+                var data = JSON.parse(request.read());
+                var target = convert(data, request.headers);
+                if (target) {
+                    var targetRequestOptions = urlParser.parse(target.url);
+                    targetRequestOptions.method = target.method || 'POST';
+                    targetRequestOptions.headers = target.headers;
+
+                    var targetRequest = http.request(targetRequestOptions, handleResponse);
+                    targetRequest.on('error', handleError);
+                    targetRequest.write(
+                        typeof target.data == 'string' ? target.data : JSON.stringify(target.data)
+                    );
+                    targetRequest.end();
+                    response.statusCode = 200;
+
+                    if (target.response) {
+                        response.send(target.response);
+                    }
+                }else {
+                    response.statusCode = 200;
+                }
+            }else {
+                response.statusCode = 400;
+            }
+        }else {
+            response.statusCode = 404;
+        }
     }
     response.end();
 }
